b317c25914c1be865e34ddf74ec3c45e56d2d7c3
Elizabeth Billings <elizabethbillings93@gmail.com>
2022-05-17 13:15:32 -0500

    Created folders, routes, and html/handlebars

---
+++ models/Post.js
@@ -0,0 +1,37 @@
+const{Model, DataTypes}= require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model{}
+Post.init({
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        primaryKey:true,
+        autoIncremented:true,
+    },
+    title: {
+            type: DataTypes.STRING,
+            allowNull:false,
+            validator: {len: [8]},
+            },
+    body: {
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+    userId:{
+            type: DataTypes.INTEGER,
+            foreignKey:true,
+            references: {
+                model:'User',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Post'
+    }
+);
+module.exports = Post
